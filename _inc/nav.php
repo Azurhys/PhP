9ff9d5d0ad042646ebfd0f516745133b34fb9fbf
Azurhys <a.merpillat@h3hitema.fr>
2023-02-27 16:35:13 +0100

    nav

--- _inc/nav.php
+++ _inc/nav.php
@@ -1,7 +1,11 @@
-<nav>
-  <ul>
-    <li><a href="/"><img src="../img/logo.png" alt="Logo" class="logo"></a></li>
-    <li><a href="/">Accueil</a></li>
-    <li><a href="/contact.php">Contact</a></li>
+<nav class="navbar">
+<div class="container">
+  <a href="/" class="navbar-brand"><img src="../img/logo.png" alt="Logo" ></a>
+  <div class="collapse navbar-collapse">
+  <ul class="navbar-nav">
+    <li  class="nav-item"><a href="/" class="nav-link">Accueil</a></li>
+    <li  class="nav-item"><a href="/contact.php" class="nav-link">Contact</a></li>
   </ul>
+  </div>
+</div>
 </nav>
