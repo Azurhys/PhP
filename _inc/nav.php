45a27b4296e433781cf77178d05fb871d0c07af5
Azurhys <a.merpillat@h3hitema.fr>
2023-02-27 16:48:44 +0100

    navbar finie exo 4

--- _inc/nav.php
+++ _inc/nav.php
@@ -1,11 +1,9 @@
-<nav class="navbar">
-<div class="container">
-  <a href="/" class="navbar-brand"><img src="../img/logo.png" alt="Logo" ></a>
-  <div class="collapse navbar-collapse">
-  <ul class="navbar-nav">
-    <li  class="nav-item"><a href="/" class="nav-link">Accueil</a></li>
-    <li  class="nav-item"><a href="/contact.php" class="nav-link">Contact</a></li>
+<nav class="navbar navbar-expand-lg m-0">
+<div class="container d-flex justify-content-start m-0">
+  <a href="/" class="navbar-brand"><img src="../img/logo.png" alt="Logo" class="logo"></a>
+  <ul class="navbar-nav d-flex">
+    <li  class="nav-item"><a href="/" class="nav-link text-secondary">Accueil</a></li>
+    <li  class="nav-item"><a href="/contact.php" class="nav-link text-secondary">Contact</a></li>
   </ul>
-  </div>
 </div>
 </nav>
