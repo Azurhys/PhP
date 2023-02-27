fd5f69c3c108a8838138ce815a278e1bced44ec1
Azurhys <a.merpillat@h3hitema.fr>
2023-02-27 16:56:55 +0100

    logo v2

--- _inc/nav.php
+++ _inc/nav.php
@@ -1,9 +1,9 @@
-<nav class="navbar navbar-expand-lg m-0">
+<nav class="navbar navbar-expand-lg bg-primary">
 <div class="container d-flex justify-content-start m-0">
   <a href="/" class="navbar-brand"><img src="../img/logo.png" alt="Logo" class="logo"></a>
   <ul class="navbar-nav d-flex">
-    <li  class="nav-item"><a href="/" class="nav-link text-secondary">Accueil</a></li>
-    <li  class="nav-item"><a href="/contact.php" class="nav-link text-secondary">Contact</a></li>
+    <li  class="nav-item"><a href="/" class="nav-link text-light">Accueil</a></li>
+    <li  class="nav-item"><a href="/contact.php" class="nav-link text-light">Contact</a></li>
   </ul>
 </div>
 </nav>
