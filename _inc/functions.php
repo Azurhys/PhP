ccc872f7c086ecd51fee2f03225ea5f62f70bba8
Azurhys <a.merpillat@h3hitema.fr>
2023-02-28 15:16:37 +0100

    connect db

--- _inc/functions.php
+++ _inc/functions.php
@@ -42,4 +42,62 @@ function isLong($value) {
     return strlen($value) >= 10;
 }
 
+function connect_db() {
+    $host = 'localhost:3306'; // remplacer par votre hôte de base de données
+    $username = 'root'; // remplacer par votre nom d'utilisateur
+    $password = ''; // remplacer par votre mot de passe
+    $database = 'videogames'; // remplacer par le nom de votre base de données
+    
+    $conn = new mysqli($host, $username, $password, $database);
+    
+    if ($conn->connect_error) {
+      die("Connexion échouée: " . $conn->connect_error);
+    }
+    
+    return $conn;
+  }
+
+  // fonction pour retourner n jeux vidéo sélectionnés aléatoirement
+function get_random_games($n) {
+    $conn = connect_db();
+    $sql = "SELECT * FROM game ORDER BY RAND() LIMIT $n";
+    $result = $conn->query($sql);
+    $games = array();
+    if ($result->num_rows > 0) {
+      while($row = $result->fetch_assoc()) {
+        $games[] = $row;
+      }
+    }
+    $conn->close();
+    return $games;
+  }
+  
+  // fonction pour retourner tous les jeux vidéo présents dans la table game
+  function get_all_games() {
+    $conn = connect_db();
+    $sql = "SELECT * FROM game";
+    $result = $conn->query($sql);
+    $games = array();
+    if ($result->num_rows > 0) {
+      while($row = $result->fetch_assoc()) {
+        $games[] = $row;
+      }
+    }
+    $conn->close();
+    return $games;
+  }
+  
+  // fonction pour retourner un jeu vidéo à l'aide de son identifiant
+  function get_game_by_id($id) {
+    $conn = connect_db();
+    $sql = "SELECT * FROM game WHERE id=$id";
+    $result = $conn->query($sql);
+    $game = null;
+    if ($result->num_rows > 0) {
+      $game = $result->fetch_assoc();
+    }
+    $conn->close();
+    return $game;
+  }
+
 ?>
---
+++ _sql/script.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS videogames; 
+
+CREATE DATABASE videogames;
+
+CREATE TABLE videogames.game (
+  id TINYINT PRIMARY KEY,
+  title VARCHAR(255),
+  description TEXT,
+  release_date DATE,
+  poster VARCHAR(255),
+  price DECIMAL(5,2)
+);
+
+INSERT INTO videogames.game (id, title, description, release_date, poster, price) VALUES
+(1, 'Super Mario Bros', 'Jeu video de plateforme', '1985-09-13', 'https://example.com/super-mario-bros.jpg', 59.99),
+(2, 'The Legend of Zelda: Ocarina of Time', 'Jeu video d\'action-aventure', '1998-11-23', 'https://example.com/zelda-ocarina-of-time.jpg', 49.99),
+(3, 'Grand Theft Auto V', 'Jeu video d\'action-aventure', '2013-09-17', 'https://example.com/gta5.jpg', 29.99);
