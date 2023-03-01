0fadbcf4ca0c8f97da80ed68fe29d515ef007937
Azurhys <a.merpillat@h3hitema.fr>
2023-03-01 10:20:25 +0100

    creation menu admin

---
+++ admin/index.php
@@ -0,0 +1,15 @@
+<?php
+  if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+  }
+  include('../_inc/header.php');
+  include('./_inc/nav.php');
+  require_once '../_inc/functions.php';
+
+?>
+
+
+
+<?php
+include('../_inc/footer.php');
+?>
